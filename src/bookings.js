207bcc4306cd0a22324fb65aa930f76b82e13ff6
louispallett <louis.pallett@outlook.com>
2023-07-15 19:39:55 +0100

    Finalised other pages (bookings and contact)

--- src/bookings.js
+++ src/bookings.js
@@ -4,7 +4,26 @@ export default function onBookings() {
     const descWrapper = document.querySelector(".desc-wrapper");
 
     descWrapper.innerHTML = 
-                            `<div> Bookings page </div>`
+                            `<form action="#">
+                            <div>
+                                <label for="name">Name</label>
+                                <input type="text" name="name" id="name">
+                            </div>
+                            <div>
+                                <label for="email">Email Address</label>
+                                <input type="email" name="email" id="email">
+                            </div>
+                            <div>
+                                <label for="number">Contact Number</label>
+                                <input type="number" name="number" id="number">
+                            </div>
+                            <div>
+                                <label for="date">Date</label>
+                                <input type="date" name="date" id="date"></input>
+                                <label for="time">Time</label>
+                                <input type="time" name="time" id="time">
+                            </div>
+                            </form>`
 
     
     homeBtn.removeAttribute("id", "on-page");
--- src/contact.js
+++ src/contact.js
@@ -4,7 +4,10 @@ export default function onContact() {
     const descWrapper = document.querySelector(".desc-wrapper");
 
     descWrapper.innerHTML = 
-                            `<div> Contact page </div>`
+                            `<h3> Contact page </h3>
+                            <p>You can contact us for any queries via: <a href="mailto: thisisnotarealemail@dontbother.com">thisisnotarealemail@dontbother.com</a></p>
+                            <p>If you're interested in work, part or full time, please let us know! We are always open to receiving applicants and even if there is no work going we can make a note. Experience is always a plus, but it is not necessary. No need to send a CV, just send an email to the link above with your name and any relevant experience, and we'll put your name down!</p>`
+                            
 
     homeBtn.removeAttribute("id", "on-page");
     bookingsBtn.removeAttribute("id", "on-page");
