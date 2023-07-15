a157d132b03567bdfceb78d64fad295bc33f5b88
louispallett <louis.pallett@outlook.com>
2023-07-15 19:53:01 +0100

    Fixed src/leaf.svg to link button making it clear which page you are on

--- src/bookings.js
+++ src/bookings.js
@@ -4,7 +4,8 @@ export default function onBookings() {
     const descWrapper = document.querySelector(".desc-wrapper");
 
     descWrapper.innerHTML = 
-                            `<form action="#">
+                            `<h3>Want to Book - great! Fill out the form below:</h3>
+                            <form action="#">
                             <div>
                                 <label for="name">Name</label>
                                 <input type="text" name="name" id="name">
@@ -23,6 +24,7 @@ export default function onBookings() {
                                 <label for="time">Time</label>
                                 <input type="time" name="time" id="time">
                             </div>
+                            <button type="submit">Submit</button>
                             </form>`
 
     
