bc5af98a25766331b298d449dcae59a5ab07933c
louispallett <louis.pallett@outlook.com>
2023-07-23 12:44:45 +0100

    CreatePage() for header

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,30 @@ import * as bootstrap from 'bootstrap'
 
 const createPage = (() => {
     const content = document.getElementById("content");
+
+    const header = document.createElement("div");
+    header.classList.add("container", "text-center");
+    header.setAttribute("id", "header");
     
-    console.log(content);
+    const headerRowArray = [];
+    const headerColArray = [];
+    
+    for(let i = 0; i < 1; i++) {
+        const headerRow = document.createElement("div");
+        headerRow.classList.add("row");
+        headerRowArray.push(headerRow);
+
+        for(let j = 0; j < 2; j++) {
+        const headerCol = document.createElement("div");
+        headerCol.classList.add("col-1");
+        headerCol.textContent = "Matcha";
+        headerColArray.push(headerCol);
+        headerRowArray[i].appendChild(headerColArray[j]);
+        }
+
+        console.log(headerRowArray);
+        header.appendChild(headerRowArray[i]);
+    }
+
+    content.appendChild(header);
 })();
