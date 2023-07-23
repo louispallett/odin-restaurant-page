b226d89389828fddbb20dbac136210925b21278d
louispallett <louis.pallett@outlook.com>
2023-07-23 11:12:08 +0100

    Began new branch. Completed file structure with bootstrap and SCSS.

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// Import our custom CSS
+import { main } from '@popperjs/core';
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+
+const createPage = (() => {
+    const content = document.getElementById("content");
+    
+    console.log(content);
+})();
