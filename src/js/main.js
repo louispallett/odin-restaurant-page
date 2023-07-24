6196264d8476805a0a04cf320d716fb98ed33ed5
louispallett <louis.pallett@outlook.com>
2023-07-24 10:50:16 +0100

    Header content

--- src/js/main.js
+++ src/js/main.js
@@ -6,26 +6,30 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
 
-const createPage = (() => {
-    const content = document.getElementById("content");
+// const createPage = (() => {
+//     const content = document.getElementById("content");
 
-    const header = document.createElement("div");
-    header.classList.add("container", "text-center");
-    header.setAttribute("id", "header");
+//     const header = document.createElement("div");
+//     header.classList.add("container", "text-center");
+//     header.setAttribute("id", "header");
    
-    for (let i = 0; i < 2; i++) { // Change loop condition to 2 for two rows
-        const headerRow = document.createElement("div");
-        headerRow.classList.add("row");
+//     for (let i = 0; i < 2; i++) { // Change loop condition to 2 for two rows
+//         const headerRow = document.createElement("div");
+//         headerRow.classList.add("row");
 
-        for (let j = 0; j < 2; j++) {
-            const headerCol = document.createElement("div");
-            headerCol.classList.add("col");
-            headerCol.textContent = "Matcha";
-            headerRow.appendChild(headerCol);
+//         for (let j = 0; j < 2; j++) {
+//             const headerCol = document.createElement("div");
+//             // headerCol.classList.add("col", `no${j}`);
+//             headerCol.textContent = "Matcha";
+//             headerRow.appendChild(headerCol);
+//             if(i == 0) {
+//                 headerCol.classList.add("col", `no${j}`);
+//             } else {
+//                 headerCol.classList.add("col", `no${j + 2}`);
+//             }
+//         }
+//         header.appendChild(headerRow);
+//     }
 
-        }
-        header.appendChild(headerRow);
-    }
-
-    content.appendChild(header);
-})();
+//     content.appendChild(header);
+// })();
